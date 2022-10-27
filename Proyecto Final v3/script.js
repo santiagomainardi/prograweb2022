40f8c8709edd04da660e004c42e31c340b0a5974
santiagomainardi <smainardi@itba.edu.ar>
2022-10-27 02:17:48 -0300

    Cambios finales HTML y CSS

---
+++ Proyecto Final v3/script.js
@@ -0,0 +1,6 @@
+// Funciones que necesito
+/* 
+1) Agregar Tarea: aparece una tarea más en la lista
+2) Done. Cuando tarea se convierte a done
+3) Convertir en hábito: esa tarea va a aparecer mañana y si la queres borrar te salta una alerta. 
+4) Borrar tarea: la tarea deja de aparecer en la lista.
