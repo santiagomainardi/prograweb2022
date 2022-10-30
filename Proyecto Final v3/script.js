2aec0f7f728f06157b59e8480f509be9a5906cf9
santiagomainardi <smainardi@itba.edu.ar>
2022-10-30 17:10:59 -0300

    JS funciones comentadas (entrega)
    JS Funciones comentadas

--- Proyecto Final v3/script.js
+++ Proyecto Final v3/script.js
@@ -1,7 +1,26 @@
 // Funciones que necesito
 /* 
-1) Agregar Tarea: aparece una tarea más en la lista
-2) Done. Cuando tarea se convierte a done
-3) Convertir en hábito: esa tarea va a aparecer mañana y si la queres borrar te salta una alerta. 
-4) Borrar tarea: la tarea deja de aparecer en la lista.
+
+LISTA DE TAREAS
+
+1. Agregar Tarea: cuando el usuario escribe una tarea y clickea el botonAdd, se crea una tarea nueva al fondo de la lista. 
+
+2. Done: cuando el usuario clickea una tarea (en cualquier lugar excepto en la "X") la tarea se convierte en #done. Se vuelve gris y se tacha. 
+
+3. Priorizar/ordenar: cuando el usuario clickea el botonOrdenar, las tareas #done se pasan abajo de la lista y las pendientes arriba de la lista.
+
+4. Borrar tarea: cuando el usuario clickea la "X", la tarea deja de aparecer en la lista.
+
+5. Mover tareas: Cuando un usuario mantiene apretado una tarea, la puede arrastrar a una posición diferente en la lista. 
+
+5. Nuevo Día: Cuando pasa el día todas las tareas se borran automáticamente.
+
+6. Si una 
+
+CALENDARIO
+
+JS tiene que averiguar la cantidad de días que tiene el mes y crear cuadrados en el calendario correspondientes. (Ejemplo: Octubre tiene 31 días, el calendario va a tener 31 cuadrados. Y así con todos los meses)
+El máximo de cuadrados que van a aparecer en pantalla al mismo tiempo son 7 (representando a los días de la semana) Para esto voy a tener que armar un grid.
+
+5) Cuando todas las tareas de la lista se completan, el día en el calendario aparece verde.
 */
