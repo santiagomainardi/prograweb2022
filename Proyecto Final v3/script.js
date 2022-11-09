5ed8030204ec9181eeb49a43cea8a1f2df97fff3
santiagomainardi <smainardi@itba.edu.ar>
2022-11-09 07:47:47 -0300

    a

--- Proyecto Final v3/script.js
+++ Proyecto Final v3/script.js
@@ -1,4 +1,47 @@
 // Funciones
+// Info date
+const dateNumber = document.getElementById('dateNumber');
+
+// Tasks Container
+const tasksContainer = document.getElementById('contenedorTareas');
+
+const setDate = () => {
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
+};
+
+const addNewTask = event => {
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+};
+
+const changeTaskState = event => {
+    event.target.classList.toggle('done');
+};
+
+const order = () => {
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach( el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el)
+    })
+    return [...toDo, ...done];
+}
+
+const renderOrderedTasks = () => {
+    order().forEach(el => tasksContainer.appendChild(el))
+}
+
+setDate();
+
+
 /* 
 LISTA DE TAREAS
 
